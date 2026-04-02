166d396e5eb829ab071f34fd71dce0b62e3aa5eb
globaluser <globaluser@hotmail.com>
2026-04-02 17:59:30 +0530

    Capstone Project V2

--- main.tf
+++ main.tf
@@ -1,5 +1,32 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+ 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+ 
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
 resource "aws_vpc" "main" { 
-  cidr_block = "10.0.0.0/22"
+  cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_subnet" "public" {
@@ -8,6 +35,12 @@ resource "aws_subnet" "public" {
   cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   map_public_ip_on_launch = true 
 } 
+
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
 
 resource "aws_subnet" "private" { 
   count = 2 
@@ -23,7 +56,31 @@ resource "aws_security_group" "alb_sg" {
     to_port = 80 
     protocol = "tcp" 
     cidr_blocks = ["0.0.0.0/0"] 
-  } 
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = aws_vpc.main.id
+ 
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_lb" "app_alb" { 
@@ -58,6 +115,11 @@ resource "aws_autoscaling_group" "app_asg" {
     id = aws_launch_template.app_lt.id 
     version = "$Latest" 
   } 
+}
+
+resource "aws_db_subnet_group" "db_subnet" {
+  name       = "db-subnet-group"
+  subnet_ids = aws_subnet.private[*].id
 }
 
 resource "aws_db_instance" "db" { 
